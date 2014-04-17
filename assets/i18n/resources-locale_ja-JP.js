812afec9e0774cd4bc886eefe7b5fa7b60cc6eed
NAMVU <vohoal_nam@VOHOAL-NAM-no-iMac-2.local>
2014-04-17 19:12:28 +0900

    i18n - add some text us

--- assets/i18n/resources-locale_ja-JP.js
+++ assets/i18n/resources-locale_ja-JP.js
@@ -8,5 +8,60 @@
         "key":"_Messages_",
         "value":"\u30e1\u30bb\u30fc\u30b8",
         "description":"Messages"
+    },
+    {
+        "key":"_Welcome_",
+        "value":"Welcome",
+        "description":"Welcome"
+    },
+    {
+        "key":"_Home_",
+        "value":"Home",
+        "description":"Home"
+    },
+    {
+        "key":"_Upload_Songs_",
+        "value":"Upload Songs",
+        "description":"Upload Songs"
+    },
+    {
+        "key":"_To_My_Dropbox_",
+        "value":"To My Dropbox",
+        "description":"To My Dropbox"
+    },
+    {
+        "key":"_Login_To_Dropbox_",
+        "value":"Login To Dropbox",
+        "description":"Login To Dropbox"
+    },
+    {
+        "key":"_My_Music_",
+        "value":"My Music",
+        "description":"My Music"
+    },
+    {
+        "key":"_Listen_",
+        "value":"Listen",
+        "description":"Listen"
+    },
+    {
+        "key":"_All_Songs_",
+        "value":"All Songs",
+        "description":"All Songs"
+    },
+    {
+        "key":"_Playlist_",
+        "value":"Playlist",
+        "description":"Playlist"
+    },
+    {
+        "key":"_Artist_",
+        "value":"Artist",
+        "description":"Artist"
+    },
+    {
+        "key":"_Album_",
+        "value":"Album",
+        "description":"Album"
     }
 ]
--- assets/i18n/resources-locale_vi-VN.js
+++ assets/i18n/resources-locale_vi-VN.js
@@ -8,5 +8,60 @@
         "key":"_Messages_",
         "value":"\u0054\u0069\u006e \u006e\u0068\u1eaf\u006e",
         "description":"Messages"
+    },
+    {
+        "key":"_Welcome_",
+        "value":"Welcome",
+        "description":"Welcome"
+    },
+    {
+        "key":"_Home_",
+        "value":"Home",
+        "description":"Home"
+    },
+    {
+        "key":"_Upload_Songs_",
+        "value":"Upload Songs",
+        "description":"Upload Songs"
+    },
+    {
+        "key":"_To_My_Dropbox_",
+        "value":"To My Dropbox",
+        "description":"To My Dropbox"
+    },
+    {
+        "key":"_Login_To_Dropbox_",
+        "value":"Login To Dropbox",
+        "description":"Login To Dropbox"
+    },
+    {
+        "key":"_My_Music_",
+        "value":"My Music",
+        "description":"My Music"
+    },
+    {
+        "key":"_Listen_",
+        "value":"Listen",
+        "description":"Listen"
+    },
+    {
+        "key":"_All_Songs_",
+        "value":"All Songs",
+        "description":"All Songs"
+    },
+    {
+        "key":"_Playlist_",
+        "value":"Playlist",
+        "description":"Playlist"
+    },
+    {
+        "key":"_Artist_",
+        "value":"Artist",
+        "description":"Artist"
+    },
+    {
+        "key":"_Album_",
+        "value":"Album",
+        "description":"Album"
     }
 ]
