24390d7ec83aed333bcd4614c51f53bfe7de5aa3
NAMVU <vohoal_nam@VOHOAL-NAM-no-iMac-2.local>
2014-04-29 16:49:12 +0900

    myProfile - add Pie

--- assets/js/myProfileController.js
+++ assets/js/myProfileController.js
@@ -2,17 +2,27 @@
 
 var myProfileController = angular.module('myProfileController', ['tc.chartjs'] );
 
-myProfileController.controller('myProfileController', ['$rootScope', '$scope', '$location' ,'$log', '$http', 'player' , 'aliceBootbox', 
-  function($rootScope , $scope , $location , $log ,$http , player , aliceBootbox ) {
+myProfileController.controller('myProfileController', ['$rootScope', '$scope', '$location' ,'$log', '$http', '$filter' , 'player' , 'aliceBootbox', 
+  function($rootScope , $scope , $location , $log ,$http , $filter , player , aliceBootbox ) {
   
     $rootScope.location = 'myProfile';
     $http.post('/getUserInfo', {} ).success(function(data, status, headers, config){
         $scope.User = data;
+        if($scope.User.dropBoxQuota ){
+            $scope.dropBoxQuota = ( $scope.User.dropBoxQuota.quota )/ 1073741824;
+            $scope.normal = ( $scope.User.dropBoxQuota.normal / $scope.User.dropBoxQuota.quota ) * 100;
+            $scope.left = ( ( $scope.User.dropBoxQuota.quota - $scope.User.dropBoxQuota.normal ) / $scope.User.dropBoxQuota.quota) * 100;
+            $scope.a = parseInt( $filter('number')($scope.normal,0) );
+            $scope.b = parseInt( $filter('number')($scope.left,0) );
+            
+            $scope.myData = [
+               { value : $scope.a, color : "#F7464A" },
+               { value : $scope.b, color : "#E2EAE9" }
+           ];
+        }
     });
     
-    $scope.myData = [
-        { value : 20, color : "#F7464A" },
-        { value : 80, color : "#E2EAE9" }
-    ];
+    
+   
     
 }]);
