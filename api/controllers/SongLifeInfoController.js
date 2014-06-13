0718ce42dceee9f9a5d157c0c273013046332c16
NAMVU <vohoal_nam@nam.cocone>
2014-06-13 18:28:56 +0900

    Add listenCnt, addCnt

--- api/controllers/SongLifeInfoController.js
+++ api/controllers/SongLifeInfoController.js
@@ -4,28 +4,36 @@ module.exports = {
     {
         SongLifeInfo.findOne( { songId : req.body.songId } , function( err , songInfo ){
             sails.log.debug( songInfo );
-            songInfo.listenCnt++;
-            songInfo.save( function( err , obj ){ 
-                if( err ){
-                    saisl.log.debug( err );
-                }
-                res.json( obj );
-            });
+            if( songInfo && songInfo.songId != '' ){
+                songInfo.listenCnt++;
+                songInfo.save( function( err , obj ){ 
+                    if( err ){
+                        saisl.log.debug( err );
+                    }
+                    res.json( obj );
+                });
+            }else{ 
+                res.json({});
+            }
         });
     },
     updateAddCnt : function ( req, res )
     {
         SongLifeInfo.findOne( { songId : req.body.songId } ).done( function( err , songInfo ){
             sails.log.debug( songInfo );
-            songInfo.addCnt++;
-            sails.log.debug(  "songInfo ====>");
-            sails.log.debug(  songInfo );
-            songInfo.save( function( err , obj ){ 
-                if( err ){
-                    saisl.log.debug( err );
-                }
-                res.json( obj );
-            });
+            if( songInfo && songInfo.songId != '' ){
+                songInfo.addCnt++;
+                sails.log.debug(  "songInfo ====>");
+                sails.log.debug(  songInfo );
+                songInfo.save( function( err , obj ){ 
+                    if( err ){
+                        saisl.log.debug( err );
+                    }
+                    res.json( obj );
+                });
+            }else{ 
+                res.json({});
+            }
         });
     }
     
