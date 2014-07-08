4a99250f214b49362519b9d41a2918d8d23c3d80
NAMVU <vohoal_nam@VOHOAL-NAM-no-iMac-2.local>
2014-07-08 12:54:05 +0900

    add maintenance page

--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -1,17 +1,26 @@
 /**
 * Allow any authenticated user.
 */
-var passport = require('passport');
+// var passport = require('passport');
+
 
 module.exports = function(req, res, next){
    
    if (req.session.user){
-        console.log("=====> Authenticated" + req.session.user );
-        return next();
+        sails.log.debug("=====> Authenticated" + req.session.user );
+
+        //CHECK APP CONFIG
+        if( !sails.config.AliceBox.MAINTENANCE ){
+        	return next();	
+        }
+        else{
+        	return res.render('maintainance');
+        }
+        
    }
    
    else{
-        console.log("=====>False Authenticate");
+        sails.log.debug("=====>False Authenticate");
         return res.redirect('/login');
    }
   
